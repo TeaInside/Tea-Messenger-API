1070ec67ee29e8a6598afc70382e544657641bef
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-13 17:47:58 +0700

    [master] Create Register API test case

---
+++ tests/API/RegisterTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tests\API;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license MIT
+ * @package \API
+ * @version 0.0.1
+ */
+class RegisterTest extends TestCase
+{
+}
