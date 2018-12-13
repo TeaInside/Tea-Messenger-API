01e128ba3841722c3c7fa6c8ef981ec728950390
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-13 14:14:19 +0700

    [master] Prepare init file

---
+++ bootstrap/init.php
@@ -0,0 +1,22 @@
+<?php
+
+if (!defined("TEA_MESSENGER_API_INIT")) {
+	define("TEA_MESSENGER_API_INIT", 1);
+
+	require __DIR__."/../config/init.php";
+
+	/**
+	 * @param string $class
+	 * @return void
+	 */
+	function teaMessengerApiInternalAutoloader(string $class): void
+	{
+		$class = str_replace("\\", "/", $class);
+		if (file_exists($f = BASEPATH."/src/classes/{$class}.php")) {
+			require $f;
+		}
+	}
+
+	require BASEPATH."/vendor/autoload.php";
+	require BASEPATH."/src/helpers.php";
+}
