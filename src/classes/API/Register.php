5948cf1fcd673b14cd07f60f4237580ecaddf4bb
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-13 17:37:36 +0700

    [master] Fix register API contract

--- src/classes/API/Register.php
+++ src/classes/API/Register.php
@@ -1,12 +1,16 @@
 <?php
 
+namespace API;
+
+use Contracts\APIContract;
+
 /**
  * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
  * @license MIT
  * @package \API
  * @version 0.0.1
  */
-class Register implements ApiContract
+class Register implements APIContract
 {
 	/**
 	 * Constructor.
