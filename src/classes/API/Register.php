6a2d838a6a22fcea7b014e2f40640ae5fe5e85cf
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-13 20:59:27 +0700

    [master] Update register API

--- src/classes/API/Register.php
+++ src/classes/API/Register.php
@@ -75,6 +75,88 @@ class Register implements APIContract
 	}
 
 	/**
+	 * @param array &$i
+	 * @return void
+	 */
+	private function validateSubmitInput(array &$i): void
+	{
+		$m = "Bad Request:";
+		$required = [
+			"first_name",
+			"last_name",
+			"email",
+			"phone",
+			"password",
+			"cpassword",
+			"captcha"
+		];
+
+		foreach ($required as $v) {
+			if (!isset($i[$v])) {
+				error_api("{$m} Field required: {$v}", 400);
+				return;
+			}
+			if (!is_string($i[$v])) {
+				error_api("{$m} Field `{$v}` must be a string", 400);
+				return;
+			}
+
+			$i[$v] = trim($i[$v]);
+		}
+
+		if ($i["captcha"] !== $this->captcha) {
+			error_api("{$m} Invalid captcha response", 400);
+			return;
+		}
+
+		unset($required, $v);
+
+		$c = strlen($i["first_name"]);
+
+		if ($c >= 200) {
+			error_api("{$m} `first_name` is too long. Please provide a name with size less than 200 bytes.", 400);
+			return;
+		}
+
+		if ($c <= 4) {
+			error_api("{$m} `first_name` is too short. Please provide a name with size more than 4 bytes.", 400);
+			return;
+		}
+
+		$c = strlen($i["last_name"]);
+
+		if ($c >= 200) {
+			error_api("{$m} `last_name` is too long. Please provide a name with size less than 200 bytes.", 400);
+			return;
+		}
+
+		if (!filter_var($i["email"], FILTER_VALIDATE_EMAIL)) {
+			error_api("{$m} \"{$i["email"]}\" is not a valid email address", 400);
+			return;
+		}
+
+		$c = strlen($i["email"]);
+
+		if ($c >= 200) {
+			error_api("{$m} `email` is too long. Please provide an email with size less than 200 bytes.", 400);
+			return;
+		}
+
+		if ($c <= 4) {
+			error_api("{$m} `email` is too short. Please provide an email with size more than 4 bytes.", 400);
+			return;
+		}
+
+		if (!preg_match("/^[0\+]\d{4,13}$/", $i["phone"])) {
+			error_api("{$m} Invalid phone number", 400);	
+			return;
+		}
+
+		unset($c, $i);
+		return;
+	}
+
+	/**
 	 * @return void
 	 */
 	private function getToken(): void
