b7e8844646151257b75f3597b212467bc9c2a642
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-13 21:00:35 +0700

    [a01] Create register API test case

--- tests/API/RegisterTest.php
+++ tests/API/RegisterTest.php
@@ -5,6 +5,8 @@ namespace tests\API;
 use tests\Curl;
 use PHPUnit\Framework\TestCase;
 
+static $testToken = null;
+
 /**
  * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
  * @license MIT
@@ -14,4 +16,24 @@ use PHPUnit\Framework\TestCase;
 class RegisterTest extends TestCase
 {
 	use Curl;
+
+	/**
+	 * @return void
+	 */
+	public function testGetToken(): void
+	{
+		global $testToken;
+		$o = $this->curl("http://localhost:8080/register.php?action=get_token");
+		$o = json_decode($o["out"], true);
+		$this->assertTrue(
+			isset(
+				$o["status"],
+				$o["data"],
+				$o["data"]["token"],
+				$o["data"]["expired"]
+			)
+		);
+		$this->assertEquals($o["status"], "success");
+		$testToken = $o["data"]["token"];
+	}
 }
