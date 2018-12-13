8617078393136208e620d299ac11c49f0dcb666c
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-13 17:41:55 +0700

    [master] Update register API

--- src/classes/API/Register.php
+++ src/classes/API/Register.php
@@ -2,6 +2,8 @@
 
 namespace API;
 
+
+use API;
 use Contracts\APIContract;
 
 /**
@@ -13,10 +15,66 @@ use Contracts\APIContract;
 class Register implements APIContract
 {
 	/**
+	 * @var string
+	 */
+	private $action;
+
+	/**
+	 * @var string
+	 */
+	private $captcha;
+
+	/**
 	 * Constructor.
 	 */
 	public function __construct()
 	{
+		if (!isset($_GET["action"])) {
+			error_api("Bad Request: Invalid action", 400);
+		}
 
+		$this->action = $_GET["action"];
+	}
+
+	/**
+	 * @return void
+	 */
+	public function run(): void
+	{
+		switch ($this->action) {
+			case "get_token":
+				$this->getToken();
+				break;
+			case "submit":
+				$this->submit();
+				break;
+			default:
+				break;
+		}
+	}
+
+	/**
+	 * @return void
+	 */
+	private function getToken(): void
+	{
+		$expired = time()+3600;
+
+		// By using this token, we don't need any session which saved at the server side.
+		print API::json001(
+			"success",
+			[
+				// Encrypted expired time and random code 6 bytes for the captcha.
+				"token" => cencrypt(json_encode(
+					[
+						"expired" => $expired,
+						"code" => rstr(6, "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM")
+					]
+				), APP_KEY),
+
+				// Show expired time.
+				"expired" => $expired
+			]
+		);
 	}
 }
