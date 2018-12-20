8a003bd6f43ab6e38c71f44fd9e01aa97cbf9363
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-20 14:06:28 +0700

    [a01] Update 'tests/init.php'

--- tests/init.php
+++ tests/init.php
@@ -2,3 +2,11 @@
 
 require_once __DIR__."/../bootstrap/init.php";
 require_once BASEPATH."/config/app.php";
+
+function truncateTable(string $table): void
+{
+	$pdo = DB::pdo();
+	$pdo->exec("SET foreign_key_checks = 0;");
+	$pdo->exec("TRUNCATE TABLE `{$table}`;");
+	unset($pdo);
+}
