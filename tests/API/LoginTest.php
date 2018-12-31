6ea568a1306276a3b9dd2faefb864db31293c3a8
Ammar Faizi <ammarfaizi2@gmail.com>
2019-01-01 00:10:39 +0700

    [master] Prepare login tests case

---
+++ tests/API/LoginTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace tests\API;
+
+use DB;
+use tests\Curl;
+use PHPUnit\Framework\TestCase;
+
+static $testToken = null;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license MIT
+ * @package \API
+ * @version 0.0.1
+ */
+class LoginTest extends TestCase
+{
+}
