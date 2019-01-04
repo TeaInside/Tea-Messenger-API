51d217008bfc2cf747af0605b305ddea8e8635a3
Ammar Faizi <ammarfaizi2@gmail.com>
2019-01-04 11:33:04 +0700

    [master] Separate edit user info class

--- src/classes/API/EditUserInfo.php
+++ src/classes/API/EditUserInfo.php
@@ -14,7 +14,7 @@ use Contracts\APIContract;
  * @package \API
  * @version 0.0.1
  */
-class Profile implements APIContract
+class EditUserInfo implements APIContract
 {
 	/**
 	 * @var array
@@ -69,41 +69,16 @@ class Profile implements APIContract
 	/**
 	 * @return void
 	 */
-	private function getUserInfo(): void
+	private function editUserInfo(): void
 	{
-		try {
-			$pdo = DB::pdo();
-			$st = $pdo->prepare(
-				"SELECT  
-					`a`.`id` AS `user_id`,`a`.`first_name`,`a`.`last_name`,
-					`a`.`gender`,
-					`a`.`registered_at`,`b`.`email`,`c`.`phone`
-				FROM `users` AS `a` 
-				INNER JOIN `emails` AS `b` ON `b`.`id` = `a`.`primary_email`
-				INNER JOIN `phones` AS `c` ON `c`.`id` = `a`.`primary_phone` 
-					WHERE `a`.`id` = :id LIMIT 1"
-			);
-			$st->execute([":id" => $this->tkn[0]]);
-			if ($st = $st->fetch(PDO::FETCH_ASSOC)) {
-				$st["photo"] = "";
-				$st["registered_at"] = date("d F Y", strtotime($st["registered_at"]));
-				foreach ($st as &$stptr) {
-					$stptr = htmlspecialchars($stptr, ENT_QUOTES, "UTF-8");
-				}
-				unset($stptr);
-				$st["user_id"] = (int)$st["user_id"];
-				print API::json001("success", $st);
-			} else {
-				error_api("Unauthorized", 401);
-				return;
-			}
-			$st = $pdo = null;
-			unset($st, $pdo);
-		} catch (PDOException $e) {
-			$e = $e->getMessage();
-			error_log($e);
-			error_api("Internal Server Error: {$e}", 500);
-			exit;
-		}
+		
+	}
+
+	/**
+	 * @return void
+	 */
+	private function validateEditUserInfoInput(): void
+	{
+		
 	}
 }
