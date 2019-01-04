5524645a8c39e3c31c60eda35226dbb6761da5cb
Ammar Faizi <ammarfaizi2@gmail.com>
2019-01-04 13:12:00 +0700

    [a01] Improve edit user info tests case

--- tests/API/Traits/EditUserInfo.php
+++ tests/API/Traits/EditUserInfo.php
@@ -9,7 +9,62 @@ namespace tests\API\Traits;
  * @version 0.0.1
  */
 trait EditUserInfo
-{
+{	
+	/**
+	 * @return array
+	 */
+	public function invalidUserData(): array
+	{
+		global $email, $first_name, $last_name, $token_session;
+
+		return [
+			[
+				[
+					"first_name" => "a",
+					"last_name" => "{$last_name} new name",
+					"gender" => "male",
+					"email" => "{$email}",
+					"phone" => "085123345567"
+				],
+				"/\`first_name\` is too short\./"
+			],
+			[
+				[
+					"first_name" => str_repeat("q", 300),
+					"last_name" => "{$last_name} new name",
+					"gender" => "male",
+					"email" => "{$email}",
+					"phone" => "085123345567"
+				],
+				"/\`first_name\` is too long\./"
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider invalidUserData
+	 * @param array  $form
+	 * @param string $mustMatch
+	 * @return void
+	 */
+	public function testInvalidEditUserInfo(array $form, string $mustMatch = null): void
+	{
+		global $token_session;
+
+		$o = $this->curl("http://localhost:8080/edit.php?action=edit_user_info",
+			[
+				CURLOPT_HTTPHEADER => [
+					"Authorization: Bearer {$token_session}"
+				],
+				CURLOPT_POST => true,
+				CURLOPT_POSTFIELDS => json_encode($form)
+			]
+		);
+
+		$this->assertEquals($o["info"]["http_code"], 400);
+		$this->assertTrue((bool)preg_match($mustMatch, $o["out"]));
+	}
+
 
 	/**
 	 * @return void
@@ -36,8 +91,6 @@ trait EditUserInfo
 			]
 		);
 
-		var_dump($o["out"]);
-
-		die;
+		$this->assertEquals($o["info"]["http_code"], 200);
 	}
 }
